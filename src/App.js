05c5aaf173d52e1fb69c2001d843a1f2a3cc0913
SLVD <965776112@qq.com>
2020-08-13 10:53:42 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+// import { Button } from "antd";
+// import logo from "./logo.svg";
+// import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { Component } from "react";
+// import Router from "../router/routes";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello app</h1>
+        {this.props.children}
+      </div>
+      // <Router />
+    );
+  }
 }
 
 export default App;
